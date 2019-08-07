f2a6ea79981f10477ba66d794841d10d6eb00bec
healthypackrat <healthypackrat@gmail.com>
2019-08-08 00:52:57 +0900

    Updates

---
+++ precache-manifest.5c121226ba0d8942f0268ff68cea7ddb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca7df82a6371132ebf4f3065d720ae4",
+    "url": "/redux-shopping-cart-example/index.html"
+  },
+  {
+    "revision": "e8e14bad90f9bf648732",
+    "url": "/redux-shopping-cart-example/static/js/2.2e7d3f35.chunk.js"
+  },
+  {
+    "revision": "cf8b8c59d33526a19577",
+    "url": "/redux-shopping-cart-example/static/js/main.69f16b51.chunk.js"
+  },
+  {
+    "revision": "278dc59cfc9aa5e13e52",
+    "url": "/redux-shopping-cart-example/static/js/runtime~main.79b82738.js"
+  }
+]);
